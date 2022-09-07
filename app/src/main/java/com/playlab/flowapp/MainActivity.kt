702b2cc522764feaeeabc0521b186ef9250e713f
João Pedro <joaoplay16@gmail.com>
2022-09-07 20:11:43 -0300

    flow implementation

--- app/src/main/java/com/playlab/flowapp/MainActivity.kt
+++ app/src/main/java/com/playlab/flowapp/MainActivity.kt
@@ -3,11 +3,15 @@ package com.playlab.flowapp
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.playlab.flowapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<MainViewModel>()
+    private val mainAdapter = MainAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +22,12 @@ class MainActivity : AppCompatActivity() {
             setHasFixedSize(true)
             adapter = MainAdapter()
         }
+
+        lifecycleScope.launch {
+            viewModel.getLanguages().collect { language ->
+                mainAdapter.addLanguage(language)
+            }
+        }
+
     }
 }
